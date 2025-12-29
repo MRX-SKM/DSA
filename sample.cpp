bd5474774eafa77d5de20c1ff50c3791e77310f4
MRX_SKM <mithun.k@s.amity.edu>
2025-12-29 18:45:00 +0530

    updated

--- sample.cpp
+++ sample.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
-int printMessage() {
+void printMessage() {
     cout << "Hello, World!" << endl;
+}
+int details(int arr[], int size){
+    int sum = 0;
+    for(int i = 0; i < size; i++){
+        sum += arr[i];
+    }
+    return sum;
 }
 int main(){
     printMessage();
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int total = details(arr, size);
+    cout << "Sum of array elements: " << total << endl;
     return 0;
 }
