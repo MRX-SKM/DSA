4b3d189c74b3e0eb59bb18a57a3d24101351e920
MRX_SKM <mithun.k@s.amity.edu>
2025-12-30 13:42:10 +0530

    added vector values

--- vectors.cpp
+++ vectors.cpp
@@ -2,10 +2,9 @@
 #include<vector>
 using namespace std;
 int main(){
-    // vector<int> v = {1,2,3,4,5};
+    vector<int> v = {1,2,3,4,5};
 
-
-    vector<int>v(5,0); //vector of size 5 with all values initialized to 0
+    // vector<int>v(5,0); //vector of size 5 with all values initialized to 0
     // loops in vectors
     for(int i : v){
         cout<< i << endl;
